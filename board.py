03fb4bacb540704cdf338299538665f62accfb9e
Charles Estabrooks <charles@estabrooks.org>
2023-05-15 09:28:20 -0400

    GUI additions

--- board.py
+++ board.py
@@ -23,47 +23,56 @@ class Grid:
     hitsRemaining = 0
     forfeit = False
     
-    def __init__(self, frame):
+    def __init__(self, canvas, CLI=False):
+        self.CLI = CLI
         self.board = [[markers['Water']]*self.width for i in range(self.height)]
         self.ships = []
         for item in v2002.items():
             self.ships.append(ship.Ship(item))
             self.hitsRemaining += item[1]
 
-        self.gridScale = 50 # Pixels per grid
-        self.frame = frame
-        self.frame.pack()
+        self.canvasScale = 50 # Pixels per grid
+        # self.canvas = canvas
+        # self.canvas.pack()
+        # self.canvas.update()
         # Canvas to display game board
-        print(f"Canvas width: {frame.winfo_reqwidth()}, height: {frame.winfo_reqheight()}")
-        self.grid = tk.Canvas(self.frame, width = frame.winfo_reqwidth(), height = frame.winfo_reqheight(), )
-        self.grid.grid(column=0, row=0, )
+        self.canvas = canvas
+        #print(f"Canvas width: {self.canvas.winfo_width()}, height: {self.canvas.winfo_height()}")
+        #self.canvas = tk.Canvas(self.canvas, width = canvas.winfo_reqwidth(), height = canvas.winfo_reqheight())
+        #self.canvas.grid(column=0, row=0)
         # print(f"Instantiated {len(self.ships)} ships")
         
     def showBoard(self, showShips = False):
-        if showShips:
-            print("\t\t\t", end='')
-        print("   1  2  3  4  5  6  7  8  9  10")
-        for y in range(self.height):
+        if self.CLI:
             if showShips:
                 print("\t\t\t", end='')
-            print(f"{chr(y+65)} ", end='')
+            print("   1  2  3  4  5  6  7  8  9  10")
+        for y in range(self.height):
+            if self.CLI:
+                if showShips:
+                    print("\t\t\t", end='')
+                print(f"{chr(y+65)} ", end='')
             for x in range(self.width):
                 # Don't show water & ships (unless we're cheating or looking at our own board)
                 if self.board[x][y] in (markers['Hit'] + markers['Miss']) or self.board[x][y] in markers['Ships'] and showShips:
-                    print(f"[{self.board[x][y]}]", end="")
+                    if self.CLI:
+                        print(f"[{self.board[x][y]}]", end="")
                     if self.board[x][y] == markers['Hit']:
-                        self.grid.create_rectangle(x*self.gridScale, y*self.gridScale, (x+1)*self.gridScale, (y+1)*self.gridScale, fill="red", outline = 'gray')
+                        self.canvas.create_rectangle(x*self.canvasScale, y*self.canvasScale, (x+1)*self.canvasScale, (y+1)*self.canvasScale, fill="red", outline = 'gray')
                     elif self.board[x][y] == markers['Miss']:
-                        self.grid.create_rectangle(x*self.gridScale, y*self.gridScale, (x+1)*self.gridScale, (y+1)*self.gridScale, fill="white", outline = 'gray')
+                        self.canvas.create_rectangle(x*self.canvasScale, y*self.canvasScale, (x+1)*self.canvasScale, (y+1)*self.canvasScale, fill="white", outline = 'gray')
                     else:
-                        self.grid.create_rectangle(x*self.gridScale, y*self.gridScale, (x+1)*self.gridScale, (y+1)*self.gridScale, fill="black", outline = 'gray')
+                        self.canvas.create_rectangle(x*self.canvasScale, y*self.canvasScale, (x+1)*self.canvasScale, (y+1)*self.canvasScale, fill="black", outline = 'gray')
                 else: # show hits & misses (and possibly ships)
-                    print(f"[ ]", end="")
+                    if self.CLI:
+                        print(f"[ ]", end="")
                     # Maybe just set the canvas background to blue?
-                    self.grid.create_rectangle(x*self.gridScale, y*self.gridScale, (x+1)*self.gridScale, (y+1)*self.gridScale, fill="blue", outline = 'gray')
-            print("")
-        self.grid.update()
-        print("---------------------------------")
+                    self.canvas.create_rectangle(x*self.canvasScale, y*self.canvasScale, (x+1)*self.canvasScale, (y+1)*self.canvasScale, fill="blue", outline = 'gray')
+            if self.CLI:
+                print("")
+        self.canvas.update()
+        if self.CLI:
+            print("---------------------------------")
 
     def listShips(self):
         for ship in self.ships:
@@ -123,12 +132,16 @@ class Grid:
         elif status == markers['Water']:
             self.board[col][row] = markers['Miss']
             print("Miss")
+            self.canvas.create_rectangle(col*self.canvasScale, row*self.canvasScale, (col+1)*self.canvasScale, (row+1)*self.canvasScale, fill="white", outline = 'gray')
+            self.canvas.update()
         elif status in markers['Ships']:
             for boat in self.ships:
                 if boat.type[0] == self.board[col][row]:
                     if boat.shoot((col, row)):
                         print("Hit!")
                         self.board[col][row] = markers['Hit']
+                        self.canvas.create_rectangle(col*self.canvasScale, row*self.canvasScale, (col+1)*self.canvasScale, (row+1)*self.canvasScale, fill="red", outline = 'gray')
+                        self.canvas.update()
                         if boat.isSunk():
                             print(f"You sunk my {boat.type}!")
                         break
--- main.py
+++ main.py
@@ -1,33 +1,121 @@
 import random, tkinter as tk
+from tkinter import font
 import board, ship
 
 winMain = tk.Tk()
 
 # Set window width/height
-winMain.wm_geometry("500x500")
+winMain.wm_geometry("1050x570")
+winMain.title("Battleship")
+
+name = input("What is your name? ")
 
 # The following two lines allow the frame to fill the window
 # We use a single row and column, but this makes them "stretchable"
 winMain.columnconfigure(0, weight=1)
 winMain.rowconfigure(0, weight=1)
+winMain.update()
 
-myFrame = tk.Frame(winMain)
-oppFrame = tk.Frame(winMain)
+boardFrame = tk.Frame(winMain, height = winMain.winfo_height(), width = winMain.winfo_width())
+boardFrame.columnconfigure(0, weight=10)
+boardFrame.columnconfigure(1, weight=10)
+boardFrame.columnconfigure(2, weight=1)
+boardFrame.rowconfigure(0, weight=2)
+boardFrame.rowconfigure(1, weight=5)
+boardFrame.rowconfigure(2, weight=50)
+boardFrame.pack()
+boardFrame.tkraise()
+boardFrame.update()
 
-for i in range(10):
-    myFrame.columnconfigure(i, weight=1)
-    myFrame.rowconfigure(i, weight=1)
-    oppFrame.columnconfigure(i, weight=1)
-    oppFrame.rowconfigure(i, weight=1)
+myLabel = tk.Label(boardFrame, text=name, font="Ariel, 14")
+myLabel.grid(column=1, row=0)
+oppLabel = tk.Label(boardFrame, text="Computer", font="Ariel, 14")
+oppLabel.grid(column=2, row=0)
 
+font = font.nametofont("TkDefaultFont")
+font.configure(family="Ariel", size=28, weight="bold")
 
-myFrame.grid(row=0, column=0, sticky="news")
-#oppFrame.grid(row=0, column=0, sticky="news")
+rowsFrame = tk.Frame(boardFrame)
+rowsFrame.grid(column=2, row=2, sticky="nesw")
+rowsFrame.columnconfigure(0, weight=1)
+rowsFrame.columnconfigure(1, weight=1)
+rowsFrame.columnconfigure(2, weight=1)
+rowsFrame.columnconfigure(3, weight=1)
+rowsFrame.columnconfigure(4, weight=1)
+rowsFrame.columnconfigure(5, weight=1)
+rowsFrame.columnconfigure(6, weight=1)
+rowsFrame.columnconfigure(7, weight=1)
+rowsFrame.columnconfigure(8, weight=1)
+rowsFrame.columnconfigure(9, weight=1)
 
-myBoard = board.Grid(myFrame)
-oppBoard = board.Grid(oppFrame)
+bdA = tk.Label(rowsFrame, text="A")
+bdB = tk.Label(rowsFrame, text="B")
+bdC = tk.Label(rowsFrame, text="C")
+bdD = tk.Label(rowsFrame, text="D")
+bdE = tk.Label(rowsFrame, text="E")
+bdF = tk.Label(rowsFrame, text="F")
+bdG = tk.Label(rowsFrame, text="G")
+bdH = tk.Label(rowsFrame, text="H")
+bdI = tk.Label(rowsFrame, text="I")
+bdJ = tk.Label(rowsFrame, text="J")
 
-#urBoard.listShips()
+bdA.grid(column=2, row=0)
+bdB.grid(column=2, row=1)
+bdC.grid(column=2, row=2)
+bdD.grid(column=2, row=3)
+bdE.grid(column=2, row=4)
+bdF.grid(column=2, row=5)
+bdG.grid(column=2, row=6)
+bdH.grid(column=2, row=7)
+bdI.grid(column=2, row=8)
+bdJ.grid(column=2, row=9)
+
+columnsFrame = tk.Frame(boardFrame)
+columnsFrame.grid(column=1, row=1, sticky="nesw")
+columnsFrame.columnconfigure(0, weight=1)
+columnsFrame.columnconfigure(1, weight=1)
+columnsFrame.columnconfigure(2, weight=1)
+columnsFrame.columnconfigure(3, weight=1)
+columnsFrame.columnconfigure(4, weight=1)
+columnsFrame.columnconfigure(5, weight=1)
+columnsFrame.columnconfigure(6, weight=1)
+columnsFrame.columnconfigure(7, weight=1)
+columnsFrame.columnconfigure(8, weight=1)
+columnsFrame.columnconfigure(9, weight=1)
+
+bd1 = tk.Label(columnsFrame, text="1")
+bd2 = tk.Label(columnsFrame, text="2")
+bd3 = tk.Label(columnsFrame, text="3")
+bd4 = tk.Label(columnsFrame, text="4")
+bd5 = tk.Label(columnsFrame, text="5")
+bd6 = tk.Label(columnsFrame, text="6")
+bd7 = tk.Label(columnsFrame, text="7")
+bd8 = tk.Label(columnsFrame, text="8")
+bd9 = tk.Label(columnsFrame, text="9")
+bd10 = tk.Label(columnsFrame, text="10")
+
+bd1.grid(column=0, row=1)
+bd2.grid(column=1, row=1)
+bd3.grid(column=2, row=1)
+bd4.grid(column=3, row=1)
+bd5.grid(column=4, row=1)
+bd6.grid(column=5, row=1)
+bd7.grid(column=6, row=1)
+bd8.grid(column=7, row=1)
+bd9.grid(column=8, row=1)
+bd10.grid(column=9, row=1)
+
+myCanvas = tk.Canvas(boardFrame, height = winMain.winfo_height(), width = winMain.winfo_width()/2)
+myCanvas.grid(column=0, row=2, sticky="nws")
+myCanvas.update()
+
+oppCanvas = tk.Canvas(boardFrame, height = winMain.winfo_height(), width = winMain.winfo_width()/2)
+oppCanvas.grid(column=1, row=2, sticky="nes")
+oppCanvas.update()
+
+myBoard = board.Grid(myCanvas)
+oppBoard = board.Grid(oppCanvas)
+
 print("Welcome to Battleship!")
 print("Randomly placing ships for you on your board.")
 
@@ -36,20 +124,12 @@ myBoard.showBoard(True)
 
 print("Randomly placing my ships (Don't peek!)")
 oppBoard.placeShips()
-#oppBoard.showBoard(False) # TESTING: Set to True for testing (or cheating!)
+oppBoard.showBoard(False) # TESTING: Set to True for testing (or cheating!)
 
 message = "Game Over"
 cheat = False
 gameOver = False
 bestGuess = ()
-
-def showMyBoard():
-    myFrame.tkraise()
-    return
-
-def showOppBoard():
-    #oppFrame.tkraise()
-    return
 
 def parseUserInput(target):
     global cheat
@@ -71,17 +151,17 @@ def parseUserInput(target):
     # Other "Cheat" codes:
     # - Sink all their ships & end game?
     # - Singk all our ships & forfeit?        
-    if not target[0].isalpha or not target[1:].isnumeric() or int(target[1:]) > 10:
+    if len(target) < 2 or not target[0].isalpha or not target[1:].isnumeric() or int(target[1:]) > 10:
         print("Invalid selection.")
         return(False)
 
     return(True)
 
 while not gameOver:
-    # Columns are A-J, rows are 1-10
+    # Rows are A-J, columns are 1-10
     # Take your turn by targeting a square, 
     # eg., 
-    showMyBoard()
+    # Player's Turn ==================================================
     validInput = False
     while not validInput:
         target = input("Target a grid squre (eg., 'E5'): ")
@@ -109,18 +189,24 @@ while not gameOver:
 
     # Their turn
     else:
-        showOppBoard()
+        # Computer's Turn ==================================================
         if bestGuess:
+            pass
+        else:
             # Computer's turn: (Dumb algorithm = random guess)
             col = random.randint(0, 9)
             row = random.randint(0, 9)
             print(f"My turn: {col}{chr(row+65)}... ", end='')
+
         myBoard.targetCell(col, row)
 
         if myBoard.gameOver():
             message += " - You lose!"
             gameOver = True
     
+        # Uncomment the following line to allow player to see stdout() messages for CLI/Text based version
+        #input("Press Enter to continue...")
+
     myBoard.showBoard(True)
     oppBoard.showBoard(cheat) # TESTING: Set to True for testing (or cheating!)
 
